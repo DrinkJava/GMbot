194135848a19f8da8acbacedea18d449404a2ba3
Nishant Maniam <nishant.maniam@gmail.com>
2017-05-21 15:15:22 -0500

    Callback

--- src/gmbot/handler.clj
+++ src/gmbot/handler.clj
@@ -3,10 +3,18 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+
+
+
+(defn callback
+  [req]
+  (str req))
+
 (defroutes app-routes
   (GET "/" [] "Hello poop")
-  (POST "/callback" request (str request))
-  (GET "/callback" request (str request))
-  (route/not-found "Not Found"))
+  (POST "/callback" request callback)
+  (POST "/" [] "You FUCK")
+  (route/not-found "Not Found\n"))
+
 
 (def app (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
